ec8124ca6fce4b0ef1bca18d779285a1796511a8
Zhang-Zizhuo <t213202446@gmail.com>
2026-07-02 18:05:04 +0800

    Update Supplementary_Figure_3_Panel_i.m

--- Supplementary Figure 3/Supplementary_Figure_3_Panel_i.m
+++ Supplementary Figure 3/Supplementary_Figure_3_Panel_i.m
@@ -54,8 +54,8 @@ ax.XTickLabel = newLabels;
 
 set(gca, 'FontSize', tick_font_size);
 
-xlabel('Rescaled duration, $t$', 'Interpreter', 'latex', 'FontSize', label_font_size);  % x 轴标签
-ylabel('Fainess, $\bar{p}$', 'Interpreter', 'latex', 'FontSize', label_font_size);  % y 轴标签
+xlabel('Rescaled duration, $t$', 'Interpreter', 'latex', 'FontSize', label_font_size);  
+ylabel('Fainess, $\bar{p}$', 'Interpreter', 'latex', 'FontSize', label_font_size);  
 
 set(gcf, 'Color', 'w');  
 set(gca, 'Color', 'w');  
