7bf2229ee9aa6981bd6a922d9a29435a4b49fddf
Zhang-Zizhuo <t213202446@gmail.com>
2026-07-02 18:06:00 +0800

    Update Figure_3_Panel_h.m

--- Figure 3/Figure_3_Panel_h.m
+++ Figure 3/Figure_3_Panel_h.m
@@ -59,7 +59,7 @@ set(gca, 'FontSize', tick_font_size);
 
 
 xlabel('Rescaled duration, $t$', 'Interpreter', 'latex', 'FontSize', label_font_size);  
-ylabel('Fainess, $\bar{p}$', 'Interpreter', 'latex', 'FontSize', label_font_size);  
+ylabel('Fairness, $\bar{p}$', 'Interpreter', 'latex', 'FontSize', label_font_size);  
 
 set(gcf, 'Color', 'w'); 
 set(gca, 'Color', 'w'); 
